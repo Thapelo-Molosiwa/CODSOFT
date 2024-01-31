84550b7286a4421a8e90595adbb00652e51f4cbd
Isaac Thapelo Molosiwa <0728921928p@gmail.com>
2024-01-31 02:47:34 +0200

    Added cards and navbar with latest router

--- e-com/Level3-Task1/src/App.jsx
+++ e-com/Level3-Task1/src/App.jsx
@@ -1,11 +1,21 @@
-import ProductLists from "../components/all products/ProductLists";
-
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from '../components/navbar/Navbar';
+import Home from '../components/home/Home';
+import Contact from '../components/contacts/Contact';
 
 function App() {
   return (
-    <>
-      <ProductLists />
-    </>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
